e3c1e7cdec65acafabadc16b1980120132b69868
Jan Kaiponen <itsjan@users.noreply.github.com>
2020-11-12 17:15:45 +0200

    reference: fs, path

---
+++ coursework/node_crash_course/reference/fs_demo.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create a folder
+
+// fs.mkdir(path.join(__dirname, 'test'), {}, err => {
+//     if (err) throw err;
+//     console.log('Folder created..')
+// });
+
+// Files
+
+// Create and write
+
+
+fs.writeFile( // this overwrites the previous contents
+    path.join(__dirname, 'test', 'hello.txt'), 
+    ' Hello World!!!', err => {
+        if (err) throw err;
+        console.log('File written to ..')
+
+        // Append to file
+
+
+        fs.appendFile( // this overwrites the previous contents
+            path.join(__dirname, 'test', 'hello.txt'), 
+            ' I love nodejs', err => {
+                if (err) throw err;
+                console.log('File written to ..');
+                
+                // Read data from file
+
+                fs.readFile(
+                    path.join(__dirname, 'test', 'hello.txt'), 
+                    'utf-8', // if not given, will return raw data
+                    (err, data) => {
+                        if (err) throw err;
+                        console.log(data);
+
+
+                        // Rename file
+
+                        fs.rename(
+                            path.join(__dirname, 'test', 'hello.txt'), 
+                            path.join(__dirname, 'test', 'helloworld.txt'),
+                            err => {
+                                if (err) throw err;
+                                console.log('File renamed ..');
+                            }
+                        )
+
+
+                    }
+                )
+
+
+        });
+});
+
+
+
+
+
+
---
+++ coursework/node_crash_course/reference/path_demo.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+// base file name
+console.log(__filename);                // complete path
+console.log(path.basename(__filename)); // just the file name
+console.log(path.dirname(__filename));  // just the directory - same as __dirname
+console.log(path.extname(__filename));  // file extension
+
+// with a path object:
+console.log(path.parse(__filename));
+/*
+{
+  root: '/',
+  dir: '/Users/johndoe/Documents/FITECH/LUT/Back-end/coursework/node_crash_course/reference',
+  base: 'path_demo.js',
+  ext: '.js',
+  name: 'path_demo'
+}
+*/
+var p = path.parse(__filename);
+console.log(p.base); // path_demo.js
+
+// concatenate paths - adds the correct delimiters
+console.log( path.join(__dirname, 'test', 'test2', 'hello.html') );
