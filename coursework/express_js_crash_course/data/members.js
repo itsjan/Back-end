16067abe392d4b126890d86dd63d8f1db2dc2a22
Jan Kaiponen <itsjan@users.noreply.github.com>
2020-12-04 16:33:54 +0200

    add PUT /api/members
    Allows email, name updates

--- coursework/express_js_crash_course/data/members.js
+++ coursework/express_js_crash_course/data/members.js
@@ -1,5 +1,5 @@
 module.exports =  [
-    { id:1, name: 'John Doe', email: 'jd@email.com', status:'a'},
-    { id:2, name: 'John Doe Sr', email: 'jd.sr@email.com', status:'a'},
-    { id:3, name: 'John Doe Jr', email: 'jd.jr@email.com', status:'a'},
+    { id:"e87ef18c-82da-40e7-a9ff-a5d75c15e74f", name: 'John Doe', email: 'jd@email.com', status:'a'},
+    { id:"029777a2-2450-4a2c-9d20-0025b570c532", name: 'John Doe Sr', email: 'jd.sr@email.com', status:'a'},
+    { id:"1acb5e3c-3665-49be-a813-8ec2b7ebd956", name: 'John Doe Jr', email: 'jd.jr@email.com', status:'a'},
 ]
--- coursework/express_js_crash_course/routes/api/members.js
+++ coursework/express_js_crash_course/routes/api/members.js
@@ -15,32 +15,61 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     // some() returns true/false
-    const exists = members.some(m => m.id === parseInt(req.params.id))
-    exists ?
-        res.json(members.filter(member => member.id === parseInt(req.params.id))) :
-        res.status(400).json({message: ` Member with ID#${req.params.id} does not exist in the database`}
-        );
+    const exists = members.some(m => m.id === req.params.id)
+    if (exists) {
+        res.json(members.filter(member => member.id === req.params.id)) ;
+    } else {
+        res.status(400).json({ message: ` Member with ID#${req.params.id} does not exist in the database` });
+    }
 });
 
 
 // Create a member
 
 router.post('/', (req, res) => {
-    const newMember = { 
-        id : uuid.v4(),
+    const newMember = {
+        id: uuid.v4(),
         name: req.body.name,
         email: req.body.email,
         status: 'active'
     }
 
     if (!newMember.name || !newMember.email) {
-        return res.status(400).json({"message" : "Please include a name and email"})
+        return res.status(400).json({ "message": "Please include a name and email" })
     }
-    
+
     // using return above, to avoid using else below
     members.push(newMember); // push to the members array
     res.json(newMember);
 
 })
+
+
+// Update member
+
+router.put('/:id', (req, res) => {
+    // some() returns true/false
+    const exists = members.some(m => m.id === req.params.id)
+
+    if (exists) {
+        const updMember = req.body;
+        members.forEach(m => {
+            if (m.id === req.params.id) {
+                m.name = updMember.name ? updMember.name : m.name;
+                m.email = updMember.email ? updMember.email : m.email;
+                return res.status(202).json({"message" : "Member updated", "member":m})
+                    ;
+            }
+        })
+
+    } else {
+        res.status(400).json({ message: ` Member with ID#${req.params.id} does not exist in the database` });
+    }
+
+
+});
+
+
+
 
 module.exports = router
