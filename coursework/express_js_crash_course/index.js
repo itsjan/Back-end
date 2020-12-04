71a8ac3c0d129e2c2c0659414b774909c92b5939
Jan Kaiponen <itsjan@users.noreply.github.com>
2020-12-04 12:37:36 +0200

    add static folder, members api (get)

--- coursework/express_js_crash_course/index.js
+++ coursework/express_js_crash_course/index.js
@@ -4,19 +4,24 @@ const path = require('path')
 const app = express();
 require('dotenv').config()
 
-// Serve files in the public folder
+members = [
+    { id:1, name: 'John Doe', email: 'jd@email.com', status:'a'},
+    { id:2, name: 'John Doe Sr', email: 'jd.sr@email.com', status:'a'},
+    { id:2, name: 'John Doe Jr', email: 'jd.jr@email.com', status:'a'},
+]
+
+app.get('/api/members', (req, res) => {
+    res.json(members);
+});
+
+// Static folder (Serve files in the public folder)
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-/*
-app.get('/', (req, res) => {
-
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-*/
-
-
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3010;
 
 app.listen(PORT,  () => console.log(`Server started on port ${PORT}`))
+
+
+
+// Quokka
+
