86291b205c743f0ff43f6acf46d362b4765c69fc
Jan Kaiponen <itsjan@users.noreply.github.com>
2020-12-15 16:41:56 +0200

    Refacture and simplify Orders API

--- your-barista/routes/orders.js
+++ your-barista/routes/orders.js
@@ -5,7 +5,7 @@ const Membership = require('../models/membership')
 const Redemption = require('../models/redemption').Redemption;//
 const membership = require('../models/membership');
 
-// POST /orders
+// Create a new order
 router.post('/', async (req, res) => {
     try {
         const newOrder = new Order(req.body);
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
     }
 })
 
-// GET /:id
+// Get order details
 router.get('/:id', async (req, res) => {
     try {
         Order.findById(req.params.id)
@@ -30,89 +30,54 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-router.post('/:id/redemption', async (req, res) => {
-    console.log("REDEMPTION");
+// Redeem drink order
+router.get('/:id/redemption/:by_membership_qr_code', async (req, res) => {
     try {
         const order_id = req.params.id;
-        //const membership_id = req.body.membership_id;
-        const qr_code_value = req.body.qr_code_value;
-        console.log({ order_id, qr_code_value })
-
-        if (!order_id ||
-            //membership_id ||
-            !qr_code_value) {
-            return res.status(400).json({
-                "inputs": {
-                    "query": req.query,
-                    "params": req.params,
-                    "body": req.body
-                },
-                "message": `Required data is missing in the request body`,
-
-
-            })
-        }
-
-        //Validate order
+        const qr_code_value = req.params.by_membership_qr_code;
 
         Order.findById(order_id)
             .then(order => {
-                console.log({ message: "ORDER FOUND", order })
-
                 if (order.is_redeemed) {
                     return res.status(409).json({ message: "Order is already redeemed and cannot be redeemed more times!" })
-                } else {
-                    Membership.findOne({ //"_id" : membership_id, 
-                        qr_code_value
-                    })
-                        .populate({ path: 'redemptions' })
-                        .then((membership) => {
-                            console.log({ TEST: "ONKO TÄSSÄ ORDER", order })
-                            if (membership.can_redeem) {
-                                const redemption = new Redemption();
-                                redemption.order_id = order_id
-                                redemption.save()
-                                //.then((redemption) => {
-                                membership.redemptions.push(redemption);
-                                membership.save()
-
-                                console.log({ redemption });
-                                //})
-
-                                Order.findById(order_id)
-                                    .then(order => {
-                                        console.log({ order })
-
-                                        if (order.redemptions) {
-                                            return res.status(409).json({ message: "Order is already redeemed and cannot be redeemed more times." })
-                                        }
-
-                                        order.redemption = redemption;
-                                        order.redeemed_by_member_name = membership.member_name;
-                                        order.redeemed_by_membership_id = membership._id; //membership_id;
-
-                                        order.save()
-                                            .then(order => {
-                                                return res.status(200).json({ message: "Order successfully redeemed", order })
-                                            })                                   
-                                    })
-
-                            } else { // member not allowed to redeem a drink at this time
-                                return res.status(403).json({ message: "Member is not allowed to redeem a drink at this time. Only 1 drink is allowed every 30 minutes, and up to 5 times per day. Please try again later.", membership });
-                            }
-                        })
                 }
 
+                Membership.findOne({ qr_code_value })
+                    .populate({ path: 'redemptions' })
+                    .then((membership) => {
+                        if (!membership.can_redeem) {
+                            return res.status(403).json({
+                                message: "Member is not allowed to redeem a drink at this time. Only 1 drink is allowed every 30 minutes, and up to 5 times per day. Please try again later.",
+                                "member_name": membership["member_name"],
+                                "valid_until": membership["valid_until"],
+                                "is_valid": membership["is_valid"],
+                                "number_of_redemptions_today": membership["number_of_redemptions_today"],
+                                "minutes_since_last_redemption": membership["minutes_since_last_redemption"],
+                                "can_redeem": membership["can_redeem"]
+                            });
+                        }
+
+                        const redemption = new Redemption({ order_id });
+                        redemption.save();
+                        membership.redemptions.push(redemption);
+                        membership.save()
+
+                        Order.findById(order_id)
+                            .then(order => {
+                                if (order.redemptions) {
+                                    return res.status(409).json({ message: "Order is already redeemed and cannot be redeemed more times." })
+                                }
+
+                                order.redemption = redemption;
+                                order.redeemed_by_member_name = membership.member_name;
+                                order.redeemed_by_membership_id = membership._id;
+                                order.save()
+                                    .then(order => {
+                                        return res.status(200).json({ message: "Order successfully redeemed", order })
+                                    })
+                            });
+                    });
             })
-
-
-
-
-
-
-
-
-
     } catch (error) {
         return res.status(500).json({ message: "INTERNAL ERROR", error })
     }
